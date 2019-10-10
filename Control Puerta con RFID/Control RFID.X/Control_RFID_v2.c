18b6351cee47bfc290e3794c00a20a240b1272bd
borjagarcia <borjagarcia@PC1405>
2019-10-10 18:32:39 +0200

    Control_RFID_v2

--- Control Puerta con RFID/Control RFID.X/Control_RFID_v2.c
+++ Control Puerta con RFID/Control RFID.X/Control_RFID_v2.c
@@ -15,7 +15,7 @@
 #pragma config CPD = OFF        
 #pragma config BOREN = OFF      
 #pragma config CLKOUTEN = OFF   
-#pragma config IESO = OFF      
+#pragma config IESO = OFF   
 #pragma config FCMEN = OFF      
 
 // CONFIG2
@@ -41,5 +41,63 @@ void interrupt Recept (void);
 
 void interrupt Recept (void)
 {
+    static char i=0; 
     
+    Buffer[i]=RCREG;
+    if(++i==14)
+        i=0;
+    else;
 }
+
+void main (void)
+{
+     //Variables Locales//
+    /*char ContraAsiic [3][12]={
+                                {'0','A','0','0','6','D','F','8','C','4','5','B'},
+                                {'0','A','0','0','6','D','7','A','B','D','A','0'},
+                                {'0','C','0','0','4','1','D','C','8','9','1','8'}
+                            };*/
+    char ContraHex[2][14]=   {
+                                {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x46,0x38,0x43,0x34,0x35,0x42,0x03},
+                                {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x37,0x41,0x42,0x44,0x41,0x30,0x03},
+                            };
+    /*char ContraHex[3][14]=   {
+                                {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x46,0x38,0x43,0x34,0x35,0x42,0x03},
+                                {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x37,0x41,0x42,0x44,0x41,0x30,0x03},
+                                {0x02,0x30,0x43,0x30,0x30,0x34,0x31,0x44,0x43,0x38,0x39,0x31,0x38,0x03}
+                            };*/
+    int comp=0;
+    //Configuración Puertos//
+    TRISA=0x00;
+
+    
+    //Configuración EUSART//
+    CREN=1;
+    SYNC=0;
+    SPEN=1; 
+    
+    //Configuración Baudios//
+    BRGH=1;
+    BRG16=0;
+    SPBRG=25;
+    
+    //Cnfiguración Interrupción EUSART//
+    GIE=1;
+    PEIE=1;
+    RCIE=1;
+    
+    //Configuración del Oscilador//
+    OSCCON=0b01101000; //4Mhz
+    
+    while (1)
+    {
+          comp=strcmp( ContraHex[2], Buffer);
+          if(comp<=0) 
+            {
+              RA2=1;
+              RA1=0;
+            }
+        else if (comp>=0) {RA1=1;RA2=0;}  
+    }
+ }
+  
