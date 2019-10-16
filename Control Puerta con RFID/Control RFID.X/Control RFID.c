0b3cc613e19ce736a839e7dad3ac50852aee4cfe
borjagarcia <borjagarcia@PC1405>
2019-10-16 16:50:11 +0200

    Control_RFID_v2

--- Control Puerta con RFID/Control RFID.X/Control RFID.c
+++ Control Puerta con RFID/Control RFID.X/Control RFID.c
@@ -15,7 +15,7 @@
 #pragma config CPD = OFF        
 #pragma config BOREN = OFF      
 #pragma config CLKOUTEN = OFF   
-#pragma config IESO = OFF      
+#pragma config IESO = OFF   
 #pragma config FCMEN = OFF      
 
 // CONFIG2
@@ -33,73 +33,114 @@
 #include <stdio.h>
 #include <string.h>
 
+//Variables//
+volatile char Buffer[13];
+
+volatile int U=0;;
+//Funciones//
+void interrupt Recept (void);
+
+void interrupt Recept (void)
+{
+    static char i=0; 
+    static char Flag=0;
+    
+    if (Flag==0)
+    {
+    while(RCREG==0x02);
+    Flag=1;
+    }    
+    if (Flag==1)
+    {
+        Buffer[i]=RCREG;
+        TXREG=Buffer[i];
+        if(++i==13)
+        {
+        i=0;
+        Flag=0;
+        U=1;
+       // __delay_ms(3000);
+        }
+        else;
+    }
+}
+
 void main (void)
-{   
-    //Variables Locales//
+{
+     //Variables Locales//
     /*char ContraAsiic [3][12]={
                                 {'0','A','0','0','6','D','F','8','C','4','5','B'},
                                 {'0','A','0','0','6','D','7','A','B','D','A','0'},
                                 {'0','C','0','0','4','1','D','C','8','9','1','8'}
                             };*/
-    char ContraHex[3][14]=   {
+    /*char ContraHex[2][14]=   {
                                 {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x46,0x38,0x43,0x34,0x35,0x42,0x03},
                                 {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x37,0x41,0x42,0x44,0x41,0x30,0x03},
+                            };*/
+    char ContraHex[2][13]=   {
+                                {0x30,0x41,0x30,0x30,0x36,0x44,0x46,0x38,0x43,0x34,0x35,0x42,0x03},
+                                {0x30,0x41,0x30,0x30,0x36,0x44,0x37,0x41,0x42,0x44,0x41,0x30,0x03},
                             };
     /*char ContraHex[3][14]=   {
                                 {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x46,0x38,0x43,0x34,0x35,0x42,0x03},
                                 {0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x37,0x41,0x42,0x44,0x41,0x30,0x03},
                                 {0x02,0x30,0x43,0x30,0x30,0x34,0x31,0x44,0x43,0x38,0x39,0x31,0x38,0x03}
                             };*/
-    
-    char Dato [14]={0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int comp=0;
+    //char com1[14]={0x02,0x30,0x41,0x30,0x30,0x36,0x44,0x46,0x38,0x43,0x34,0x35,0x42,0x03};
+    char com2[13]={0,0,0,0,0,0,0,0,0,0,0,0,0};
     //Configuración Puertos//
-    TRISA1=0;
-    TRISA2=0;
+    TRISA=0x00;
+
     
-    //Timer1//
-    T1CON=0b00110001;
-   //Configuración EUSART//
+    //Configuración EUSART//
     CREN=1;
     SYNC=0;
     SPEN=1; 
     
     //Configuración Baudios//
     BRGH=1;
-    BRG16=0;
-    SPBRG=25;
+    BRG16=1;
+    SPBRG=103;
+    TXEN = 1;
+    //Cnfiguración Interrupción EUSART//
+    GIE=1;
+    PEIE=1;
+    RCIE=1;
     
     //Configuración del Oscilador//
     OSCCON=0b01101000; //4Mhz
     
-    while(1)
+    while (1)
     {
-        TMR1IF=0;
-        
-       /* for (int p=0;p<14;p++)
-        {
-            Dato[p]=0;
-        } */
-        
-        for(int i=0;i<14;i++)
-        {
-            while (RCIF==0 );
-            Dato[i]=RCREG;
-            __delay_ms(500);
-        }
-        
-      /* for(int z=0;z<2;z++)
-        {
-           comp=strcmp( ContraHex[z], Dato);
-        }*/
-        comp=strcmp( ContraHex[2], Dato);
-          if(comp<0) 
-            {
-            RA1=0;       
-            RA2=1;
-            __delay_ms(5000);
-             RA2=0;
+        static int x=0;
+        U=0;
+        //memset(Buffer,'\0',13);
+        PORTA=0x00;
+        while (U==0);
+        GIE=0;
+        do { 
+           comp=strncmp ( ContraHex[x], Buffer,13);
+           if(++x==2)x=0;
+        } while(comp!=0 && x>0);
+           
+        if (comp==0)
+           {
+              PORTA=0b00000010;
+              __delay_ms(3000);
+              //memset(Buffer,'\0',13);
             }
-        else if (comp>0) {RA1=1;}  
+       else //if(comp!=0)
+            {
+               PORTA=0b00000001;
+               __delay_ms(1000);    
+            }
+        GIE=1;
     }
-}
+
+      
+
+       
+   
+ }
+  
